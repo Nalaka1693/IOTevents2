0d2233e12873ed533e7e0ffd27d6dea6de75cf87
Nalaka1693 <nalaka1693@gmail.com>
2015-11-27 21:52:18 +0530

    motion constructor ok

--- events/motionSense.js
+++ events/motionSense.js
@@ -2,25 +2,26 @@ module.exports = {
     Motion: function(hubID, sensorID) {
         this.hubID = hubID;
         this.sensorID = sensorID;
+        this.interval = 5000;
         var dataArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
         this.motionFunc = function(data) {
-            fillArray(data);
+            fillArray(data, dataArray);
 
-            if (eval(dataArray.join('+')) > -1) {
+            if (eval(dataArray.join('+')) > 1) {
                 var date = new Date();
-                printDate(date);
+                printDate(date, sensorID);
             }
         };
     }
 };
 
-function printDate(date) {
-    console.log("Motion detected on " + date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " at " +
+function printDate(date, num) {
+    console.log("Motion detected on motion sensor No: " + num + " on " + date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " at " +
         date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
 }
 
-function fillArray(data) {
+function fillArray(data, dataArray) {
     for (var i = dataArray.length-1; i >= 0 ; i--) {
         dataArray[i] = dataArray[i-1];
     }
--- eventsIOT.js
+++ eventsIOT.js
@@ -9,83 +9,44 @@ var acceleSense = require('./events/acceleSense');
 
 var router = express.Router();
 
-var mInterval = 1000;
-var dInterval = 60000;
-var tInterval = 2000;
-var aInterval = 1000;
-var hInterval = 60000 * 5;
-var wInterval = 60000 * 5;
+var motionArray = [];
 
-var motionData = 0;
-var doorData = 0;
-var tempData = 30;
-var humidData = 51;
-var waterData = [0, 0, 0, 0];
-var acceleData = [0, 0, 0];
+router.get('/newmotion/:hubid/:sensorid', function(req, res) {
+    var hubid = req.params.hubid;
+    var sensorid = req.params.sensorid;
 
+    var motionSensor = new motionSense.Motion(hubid, sensorid);
+    motionArray.push(motionSensor);
+    res.send("new motion");
 
-router.get('/minterval/:interval', function(req, res) {
-    mInterval = req.params.interval;
-    res.send(mInterval);
-    console.log("got " + mInterval);
+    console.log(motionArray.length);
 });
-router.get('/dinterval/:interval', function(req, res) {
-    dInterval = req.params.interval;
-    console.log("got " + dInterval);
-});
-router.get('/ainterval/:interval', function(req, res) {
-    aInterval = req.params.interval;
-    console.log("got " + aInterval);
-});
-router.get('/hinterval/:interval', function(req, res) {
-    hInterval = req.params.interval;
-    console.log("got " + hInterval);
-});
+router.get('/minterval/:hubid/:sensorid/:interval', function(req, res) {
+    var hubid = req.params.hubid;
+    var sensorid = req.params.sensorid;
+    var interval = req.params.interval;
 
-/* motion   0
- * door     1
- * temp     2
- * humidity 3 
- * water    4
- * accele   5
- */
-router.get('/data', function(req, res) {
-    var eventId = req.param('eid');
-
-    if (eventId == 0) {
-        motionData = req.param('data');
-    } else if (eventId == 1) {
-        doorData = req.param('data');
-    } else if (eventId == 2) {
-        tempData = req.param('data');
-    } else if (eventId == 3) {
-        humidData = req.param('data');
-    } else if (eventId == 4) {
-        waterData = req.param('data');
-        res.send(waterData);
-    } else if (eventId == 5) {
-        acceleData = req.param('data');
+    for (var i = 0; i < motionArray.length; i++) {
+        if (motionArray[i].hubID == hubid && motionArray[i].sensorID == sensorid) {
+            motionArray[i].interval = interval;
+            break;
+        }
     }
-    console.log("motion", motionData, "door", doorData, "temp", tempData);
+});
+router.get('/mdata/:hubid/:sensorid/:data', function(req, res) {
+    var hubid = req.params.hubid;
+    var sensorid = req.params.sensorid;
+    var data = req.params.data;
+
+    for (var i = 0; i < motionArray.length; i++) {
+        if (motionArray[i].hubID == hubid && motionArray[i].sensorID == sensorid) {
+            break
+        }
+    }
+    setInterval(function() {
+        motionArray[i].motionFunc(data);
+    }, motionArray[i].interval);
 });
 
-setInterval(function () {
-    motionSense.motion(motionData, mInterval);
-}, mInterval);
-setInterval(function () {
-    doorSense.doorOpenCls(doorData);
-}, dInterval);
-setInterval(function () {
-    tempSense.temp(tempData);
-}, tInterval);
-setInterval(function () {
-    humidSense.humidity(humidData);
-}, hInterval);
-setInterval(function () {
-    tankWaterSense.waterLvl(waterData);
-}, wInterval);
-setInterval(function () {
-    acceleSense.acceleration(acceleData);
-}, aInterval);
 
 module.exports = router;
